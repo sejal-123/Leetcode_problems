df315998351dc9a99e9e9dc9814a166c319b23af
sejal-123 <55838624+sejal-123@users.noreply.github.com>
2024-01-16 23:57:22 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (30.53%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,31 +1,46 @@
 class Solution {
     public void sortColors(int[] nums) {
-        // int arr[] = new int[nums.length];
+        // TreeMap<Integer, Integer> map = new TreeMap<>();
         // for (int j = 0; j < nums.length; j++) {
-        //         arr[j] = nums[j];
-        //     }
-        // int k = 0;
-        // for (int i = 0; i < 3; i++) {
-        //     for (int j = 0; j < arr.length; j++) {
-        //         if (arr[j] == i) {
-        //             nums[k] = i;
-        //         }
+        //     if(map.get(nums[j]) == null) {
+        //         map.put(nums[j], 1);
+        //     } else {
+        //         map.put(nums[j], map.get(nums[j])+1);
         //     }
         // }
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        for (int j = 0; j < nums.length; j++) {
-            if(map.get(nums[j]) == null) {
-                map.put(nums[j], 1);
-            } else {
-                map.put(nums[j], map.get(nums[j])+1);
-            }
+        // int j = 0;
+        // for (Map.Entry<Integer, Integer> m: map.entrySet()) {
+        //     int key = m.getKey();
+        //     for (int k = 0; k < m.getValue(); k++) {
+        //         nums[j] = m.getKey();
+        //         j += 1;
+        //     }
+        // }
+        int zeroes = 0, ones=0, twos=0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0)
+                zeroes++;
+            else if (nums[i] == 1)
+                ones++;
+            else
+                twos++;
         }
-        int j = 0;
-        for (Map.Entry<Integer, Integer> m: map.entrySet()) {
-            int key = m.getKey();
-            for (int k = 0; k < m.getValue(); k++) {
-                nums[j] = m.getKey();
-                j += 1;
+        int p = 0, q = 0, r = 0, i = 0;
+        while (i < nums.length){
+            while(p < zeroes) {
+                nums[i] = 0;
+                i++;
+                p++;
+            }
+            while(q < ones) {
+                nums[i] = 1;
+                i++;
+                q++;
+            }
+            while(r < twos) {
+                nums[i] = 2;
+                i++;
+                r++;
             }
         }
     }
