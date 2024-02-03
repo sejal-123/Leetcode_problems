0f0fcc79e154bcbcf7f512ba59f3a7a0d41f7e53
sejal-123 <55838624+sejal-123@users.noreply.github.com>
2024-02-04 01:28:11 +0530

    Time: 4 ms (59.16%), Space: 43.4 MB (52.07%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,20 +1,14 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        ArrayList<String> ch = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
         char ch1[] = s.toCharArray();
         for (char c : ch1) {
-            if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90) || (c >= 48 && c <= 57)) {
-                ch.add(String.valueOf(c));
+            if (Character.isLetterOrDigit(c)) {
+                sb.append(Character.toLowerCase(c));
             }
         }
-        int p = 1, i = 0, j = ch.size() - 1;
-        while (i <= j) {
-            if (!ch.get(i).toLowerCase().equals(ch.get(j).toLowerCase())) {
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
+        if (sb.toString().equals(sb.reverse().toString()))
+            return true;
+        return false;
     }
 }
