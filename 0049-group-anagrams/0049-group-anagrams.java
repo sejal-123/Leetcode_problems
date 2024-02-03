885d8ba689a670198f2f675ed322810720b48017
sejal-123 <55838624+sejal-123@users.noreply.github.com>
2024-02-04 00:28:51 +0530

    Time: 470 ms (5%), Space: 47.5 MB (52.99%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        ArrayList<String> temp = new ArrayList<>();
+        for (int i = 0; i < strs.length; i++) {
+            char ch[] = strs[i].toCharArray();
+            Arrays.sort(ch);
+            temp.add(new String(ch));
+        }
+        int t[] = new int[strs.length];
+        List<List<String>> ans =  new ArrayList<List<String>>();
+        for (int i = 0; i < temp.size(); i++) {
+            ArrayList<String> temp1 = new ArrayList<>();
+            if (t[i] == 0) {
+                temp1.add(strs[i]);
+                t[i] = 1;
+            }
+            for (int j = i+1; j < temp.size(); j++) {
+                if (temp.get(i).equals(temp.get(j)) && t[j] == 0) {
+                    temp1.add(strs[j]);
+                    t[j] = 1;
+                }
+            }
+            if (temp1.size() > 0)
+                ans.add(temp1);
+        }
+        return ans;
+    }
+}
