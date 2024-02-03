4d501c751b9ce785e78ca4b04dcde6f3b9bf2a56
sejal-123 <55838624+sejal-123@users.noreply.github.com>
2024-02-04 00:45:58 +0530

    Time: 6 ms (91.92%), Space: 47.7 MB (43.04%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,28 +1,16 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         ArrayList<String> temp = new ArrayList<>();
-        for (int i = 0; i < strs.length; i++) {
-            char ch[] = strs[i].toCharArray();
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s: strs) {
+            char ch[] = s.toCharArray();
             Arrays.sort(ch);
-            temp.add(new String(ch));
-        }
-        int t[] = new int[strs.length];
-        List<List<String>> ans =  new ArrayList<List<String>>();
-        for (int i = 0; i < strs.length; i++) {
-            ArrayList<String> temp1 = new ArrayList<>();
-            if (t[i] == 0) {
-                temp1.add(strs[i]);
-                t[i] = 1;
+            String sortedWord = new String(ch);
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
             }
-            for (int j = i+1; j < strs.length; j++) {
-                if (temp.get(i).equals(temp.get(j)) && t[j] == 0) {
-                    temp1.add(strs[j]);
-                    t[j] = 1;
-                }
-            }
-            if (temp1.size() > 0)
-                ans.add(temp1);
+            map.get(sortedWord).add(s);
         }
-        return ans;
+        return new ArrayList<>(map.values());
     }
 }
