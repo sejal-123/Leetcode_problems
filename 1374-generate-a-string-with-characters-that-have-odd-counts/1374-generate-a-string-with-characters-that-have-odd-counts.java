287b6e7bb2b0002fdebf5f6bd2cc8323fc7edbb7
sejal-123 <55838624+sejal-123@users.noreply.github.com>
2024-02-05 23:26:38 +0530

    Time: 4 ms (17.02%), Space: 43.9 MB (20.25%) - LeetHub

--- 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.java
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.java
@@ -9,19 +9,7 @@ class Solution {
             }
             return ans;
         }
-         else if (n%2 == 0 && n/2 % 2 != 0) {
-            int i = 0;
-            while(i < n/2) {
-                ans += "a";
-                i++;
-            }
-            while(i < n) {
-                ans += "b";
-                i++;
-            }
-            return ans;
-        }
-        else if (n%2 == 0 && n/2 % 2 == 0) {
+        else {
             int i = 0;
             while(i < n-1) {
                 ans += "a";
@@ -31,18 +19,6 @@ class Solution {
                 ans += "b";
                 i++;
             }
-            return ans;
-        } else {
-            int i = 0;
-            while(i < (n-1)/2) {
-                ans += "a";
-                i++;
-            }
-            while(i < n-1) {
-                ans += "b";
-                i++;
-            }
-            ans += "c";
             return ans;
         }
     }
