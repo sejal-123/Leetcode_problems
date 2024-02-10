722207f157fcab369c6d8b249225acacd3c961c0
sejal-123 <55838624+sejal-123@users.noreply.github.com>
2024-02-10 16:27:44 +0530

    Time: 5 ms (60.84%), Space: 45.1 MB (16.78%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -5,19 +5,17 @@ class Solution {
         map.put('X', 10);   map.put('L', 50);
         map.put('C', 100);  map.put('D', 500);
         map.put('M', 1000);
-        int ans = 0, f = 0, i = 0;
+        int ans = 0, i = 0;
         while(i < s.length() - 1) {
             if (map.get(s.charAt(i)) < map.get(s.charAt(i+1))) {
                 ans += map.get(s.charAt(i+1)) - map.get(s.charAt(i));
-                f = 1;
                 i += 2;
             } else {
                 ans += map.get(s.charAt(i));
-                f = 0;
                 i++;
             }
         }
-        if (f == 0 || i == s.length() - 1) {
+        if (i == s.length() - 1) {
             ans += map.get(s.charAt(s.length() - 1));
         }
         return ans;
