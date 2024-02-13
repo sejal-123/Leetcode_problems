4c07bbf31ee78a9b8f1005b2e8b140a1cc9e9d61
sejal-123 <55838624+sejal-123@users.noreply.github.com>
2024-02-13 09:12:30 +0530

    Time: 17 ms (38.22%), Space: 45.1 MB (77.85%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -5,23 +5,22 @@ class Solution {
         }
         StringBuilder ans = new StringBuilder();
         Map<Character, Integer> map = new HashMap<>();
+        TreeMap<Integer, List<Character>> lst = new TreeMap<>(Collections.reverseOrder());
         for(int i = 0; i < s.length(); i++) {
             map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)+1);
         }
-        List<Character>[] arr = new List[s.length()+1];
-        for (Character key: map.keySet()) {
-            int val = map.get(key);
-            if (arr[val] == null)
-                arr[val] = new ArrayList<>();
-            arr[val].add(key);
+        for (Map.Entry<Character, Integer> entry: map.entrySet()) {
+            if (!lst.containsKey(entry.getValue())) {
+                lst.put(entry.getValue(), new ArrayList<>());
+            }
+            lst.get(entry.getValue()).add(entry.getKey());
         }
-        for (int i = arr.length-1; i >= 0; i--){
-            if (arr[i] != null) {
-                for (char ch: arr[i]) {
-                    ans.append(String.valueOf(ch).repeat(map.get(ch)));
-                }
+        for (Map.Entry<Integer, List<Character>> entry: lst.entrySet()) {
+            for (char c: entry.getValue()) {
+                ans.append(String.valueOf(c).repeat(entry.getKey()));
             }
         }
+        
         return ans.toString();
     }
 }
