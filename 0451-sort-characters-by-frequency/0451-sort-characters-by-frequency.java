3d43e4152ac9a4dc51c6b79156e4481fc4c1c77e
sejal-123 <55838624+sejal-123@users.noreply.github.com>
2024-02-13 09:02:00 +0530

    Time: 15 ms (59.88%), Space: 45.5 MB (53.61%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String frequencySort(String s) {
+        if (s.length() == 0) {
+            return "";
+        }
+        StringBuilder ans = new StringBuilder();
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)+1);
+        }
+        List<Character>[] arr = new List[s.length()+1];
+        for (Character key: map.keySet()) {
+            int val = map.get(key);
+            if (arr[val] == null)
+                arr[val] = new ArrayList<>();
+            arr[val].add(key);
+        }
+        for (int i = arr.length-1; i >= 0; i--){
+            if (arr[i] != null) {
+                for (char ch: arr[i]) {
+                    ans.append(String.valueOf(ch).repeat(map.get(ch)));
+                }
+            }
+        }
+        return ans.toString();
+    }
+}
