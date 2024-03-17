5eb6c469dde3c21622c475680a9f0db58ad099fb
sejal-123 <55838624+sejal-123@users.noreply.github.com>
2024-03-17 20:22:58 +0530

    Time: 9 ms (94.7%), Space: 56.9 MB (45.21%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -10,19 +10,40 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        ListNode n = head;
-        ArrayList<Integer> lst = new ArrayList<>();
-        int len = 0;
-        while(n != null) {
-            lst.add(n.val);
-            n = n.next;
+        if (head == null || head.next == null) {
+            return head;
         }
-        n = head;
-        Collections.sort(lst);
-        for(Integer i: lst) {
-           n.val = i;
-           n = n.next;
+        ListNode prev = null, slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        return head;
+        prev.next = null;
+        ListNode l1 = sortList(head);
+        ListNode l2 = sortList(slow);
+
+        return merge(l1, l2);
+    }
+
+    ListNode merge(ListNode l1, ListNode l2){
+        ListNode l = new ListNode(0), p = l;
+        while(l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                p.next = l1;
+                l1 = l1.next;
+            } else {
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+        if (l1 != null) {
+            p.next = l1;
+        }
+        if (l2 != null) {
+            p.next = l2;
+        }
+        return l.next;
     }
 }
