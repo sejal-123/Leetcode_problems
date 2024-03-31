6902f33681717f45c89060db86c3c4f755d0071b
sejal-123 <55838624+sejal-123@users.noreply.github.com>
2024-03-31 17:14:01 +0530

    Time: 1453 ms (96.15%), Space: 0B (100%) - LeetHub

---
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+-- select s.student_id, s.student_name, subject_name, count(e.subject_name) attended_exams from Students s left join Examinations e on s.student_id = e.student_id group by e.student_id, e.subject_name;
+select s.student_id, student_name, sb.subject_name, count(e.student_id) attended_exams from Students s cross join Subjects sb left join Examinations e on s.student_id = e.student_id and sb.subject_name = e.subject_name group by s.student_id, s.student_name, sb.subject_name order by s.student_id, sb.subject_name;
