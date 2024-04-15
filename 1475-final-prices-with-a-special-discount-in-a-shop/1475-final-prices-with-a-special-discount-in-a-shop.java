430a20f6836632f6f39a8382f78c1a3dcc1c7950
sejal-123 <55838624+sejal-123@users.noreply.github.com>
2024-04-15 22:55:25 +0530

    Time: 24 ms (5.88%), Space: 44.4 MB (8.26%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        if (prices.length == 0 || prices.length == 1)
+            return prices;
+        else {
+            int temp[] = new int[prices.length];
+            temp[prices.length-1] = prices[prices.length-1];
+            int i = prices.length-2, j = 0;
+            while (i >= 0) {
+                j = i+1;
+                if (prices[i] >= prices[j]) {
+                    temp[i] = prices[i] - prices[j];
+                    System.out.println(temp);
+                    i--;
+                } else {
+                    while (j < prices.length) {
+                        if (prices[i] >= prices[j]) {
+                            temp[i] = prices[i] - prices[j];
+                            System.out.println(temp);
+                            i--;
+                            break;
+                        }
+                        j++;
+                    }
+                    if (j == prices.length) {
+                        temp[i] = prices[i];
+                        i--;
+                    }
+                }
+            }
+            return temp;
+        }
+    }
+}
