70d259420bcb001f7d2535ce9dedeeb7ced91434
sejal-123 <55838624+sejal-123@users.noreply.github.com>
2024-04-16 23:03:42 +0530

    Time: 3 ms (59.1%), Space: 42.3 MB (34.74%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,35 +1,18 @@
 class Solution {
     public String makeGood(String s) {
-        String s1 = "", s2 = "";
-        s2 = s;
-        while (true) {
-            s1 = reduceString(s2);
-            s2 = reduceString(s1);
-            if (s1.equals(s2)) {
-                break;
-            } else if (s2.equals("")) {
-                break;
-            }
-        }
-        return s2;     
+        Stack<Character> stack = new Stack<>();
 
-    }
-
-    public String reduceString(String s) {
-        char ch[] = s.toCharArray();
-        if (ch.length == 0 || ch.length == 1) {
-            return s;
-        }
-        int i = ch.length - 2, j = ch.length - 1;
-        while (i >= 0) {
-            if (((Character.isUpperCase(ch[i]) && Character.isLowerCase(ch[j])) ||(Character.isUpperCase(ch[j]) && Character.isLowerCase(ch[i]))) && Character.toLowerCase(ch[i]) == Character.toLowerCase(ch[j])) {
-                s = s.substring(0, i) + s.substring(j+1, s.length());
-                return s;
+        for (char ch: s.toCharArray()) {
+            if (!stack.isEmpty() && Math.abs(ch - stack.peek()) == 32) {
+                stack.pop();
             } else {
-                i--;
-                j--;
+                stack.push(ch);
             }
         }
-        return s;
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()) {
+            sb.insert(0, stack.pop());
+        }
+        return sb.toString();
     }
 }
