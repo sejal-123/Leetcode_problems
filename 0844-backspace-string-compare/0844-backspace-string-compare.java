4ee83e03ba11585022ec88fda4abdfb173e1cb37
sejal-123 <55838624+sejal-123@users.noreply.github.com>
2024-04-17 23:31:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -3,19 +3,15 @@ class Solution {
         Stack<Character> st1 = new Stack<>();
         Stack<Character> st2 = new Stack<>();
         for (char ch: s.toCharArray()) {
-            System.out.println(ch);
             if (ch == '#') {
                 if (st1.isEmpty()) {
                     continue;
                 } else 
-                System.out.println(st1.pop());
             }
             else {
-                System.out.println(ch);
                 st1.push(ch);
             }
         }
-        System.out.println(st1);
         for (char ch: t.toCharArray()) {
             if (ch == '#') {
                 if (st2.isEmpty()) {
@@ -34,7 +30,6 @@ class Solution {
         while(!st2.isEmpty()) {
             t = st2.pop() + t;
         }
-        System.out.println(s + " " + t);
         return s.equals(t);
     }
 }
