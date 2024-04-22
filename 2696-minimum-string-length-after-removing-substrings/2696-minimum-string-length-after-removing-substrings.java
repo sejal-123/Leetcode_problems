bf9d9ee800e2ac186dc3950fe2411ea61ef9a7f4
sejal-123 <55838624+sejal-123@users.noreply.github.com>
2024-04-22 23:39:11 +0530

    Time: 10 ms (13.65%), Space: 45.3 MB (6.77%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
@@ -4,16 +4,21 @@ class Solution {
     }
 
     public String reduceStringLength(String s) {
-        if (s.contains("AB")) {
-            int index = s.indexOf("AB");
+        if (s.contains("AB") || s.contains("CD")) {
+            int index = -1;
+            if (s.contains("AB")) {
+                index = s.indexOf("AB");
+            } else {
+                index = s.indexOf("CD");
+            }
             s = s.substring(0, index) + s.substring(index+2, s.length());
             s = reduceStringLength(s);
         }
-        if (s.contains("CD")) {
-            int index = s.indexOf("CD");
-            s = s.substring(0, index) + s.substring(index+2, s.length());
-            s = reduceStringLength(s);
-        }
+        // if (s.contains("CD")) {
+        //     int 
+        //     s = s.substring(0, index) + s.substring(index+2, s.length());
+        //     s = reduceStringLength(s);
+        // }
         return s;
     }
 }
